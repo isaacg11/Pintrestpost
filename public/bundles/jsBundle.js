6f270f281892963b0913480060fa54e4658061dc
snoopy223 <idctony@gmail.com>
2015-08-03 16:16:32 -0700

    home html finished

---
+++ public/bundles/jsBundle.js
@@ -0,0 +1,39 @@
+(function() {
+	'use strict';
+	angular.module('app', ['ui.router'])
+	.config(Config);
+	Config.$inject = ['$stateProvider', '$urlRouterProvider'];
+	function Config($stateProvider, $urlRouterProvider) {
+		$stateProvider.state('Home',{
+			url: '/',
+			templateUrl: 'views/home.html'
+		});
+		$urlRouterProvider.otherwise('/');
+	}
+})();
+
+(function() {
+	'use strict';
+	angular.module('app')
+	.controller('HomeController', HomeController);
+
+	HomeController.$inject = [];
+
+	function HomeController() {
+		var vm = this;
+		vm.title = 'Welcome to our Ap!';
+	}
+})();
+(function() {
+	'use strict';
+	angular.module('app')
+	.factory('HomeFactory', HomeFactory);
+
+	HomeFactory.$inject = ['$http', '$q'];
+
+	function HomeFactory($http, $q) {
+		var o = {};
+		
+		return o;
+	}
+})();
